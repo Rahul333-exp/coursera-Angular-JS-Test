a50c2e414110796b5a9abd64e3a76a3c0f03ae96
RAHUL THAKKAR <67954885+Rahul333-exp@users.noreply.github.com>
2020-09-02 21:57:09 +0530

    Add files via upload

--- module4_solution/src/menu/items.component.js
+++ module4_solution/src/menu/items.component.js
@@ -1,8 +1,8 @@
 (function() {
     'use strict';
     angular.module('MenuApp')
-        .component('categories', {
-            templateUrl: 'src/menu/templates/categories.template.html',
+        .component('items', {
+            templateUrl: 'src/menu/templates/items.template.html',
             bindings: {
                 list: '<'
             }
